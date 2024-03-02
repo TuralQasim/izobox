5c204ac829b982cd9a645742c9fa3ffb7d179028
TuqalQasim <qasimovtural572@gmail.com>
2024-03-02 18:59:08 +0400

    fix info img

--- src/components/infoImage/InfoImage.jsx
+++ src/components/infoImage/InfoImage.jsx
@@ -1,19 +1,92 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./infoImage.css";
-import InfoImg from "../infoImg/InfoImg";
 
-const InfoImage = () => {
+const InfoImage = ({ dispatch }) => {
   return (
     <div className="info_image">
-      <InfoImg src="./public/infoImage1.png" />
-      <InfoImg src="./public/infoImage2.png" />
-      <InfoImg src="./public/infoImage3.png" />
-      <InfoImg src="./public/infoImage4.png" />
-      <InfoImg src="./public/infoImage5.png" />
-      <InfoImg src="./public/infoImage6.png" />
-      <InfoImg src="./public/infoImage7.png" />
-      <InfoImg src="./public/infoImage8.png" />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage1.png",
+          });
+        }}
+        src="./public/infoImage1.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage2.png",
+          });
+        }}
+        src="./public/infoImage2.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage3.png",
+          });
+        }}
+        src="./public/infoImage3.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage4.png",
+          });
+        }}
+        src="./public/infoImage4.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage5.png",
+          });
+        }}
+        src="./public/infoImage5.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage6.png",
+          });
+        }}
+        src="./public/infoImage6.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage7.png",
+          });
+        }}
+        src="./public/infoImage7.png"
+        alt=""
+      />
+      <img
+        onClick={() => {
+          dispatch({
+            type: "INFOIMAGE",
+            payload: "./public/infoImage8.png",
+          });
+        }}
+        src="./public/infoImage8.png"
+        alt=""
+      />
     </div>
   );
 };
-export default InfoImage;
+const t = (a) => a;
+export default connect(t)(InfoImage);
